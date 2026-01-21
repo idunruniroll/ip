44c8f440339cf4751b0f5f70cf6cb0dd8fb33d3d
idunruniroll <taan.yi.qian@gmail.com>
2026-01-22 01:07:48 +0800

    Level-2 add tasks input by user and return all tasks inputted

--- src/main/java/Chad.java
+++ src/main/java/Chad.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class Chad {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String[] taskList = new String[100];
+        int taskCounter = 0;
+
+        //intro
         String logo = "       _               _ \n"
                 + "      | |             | |\n"
                 + "   ___| |__   __ _  __| |\n"
@@ -12,9 +16,10 @@ public class Chad {
         System.out.println("___________________________________");
         System.out.println("Hello! I'm Chad");
         System.out.println("What can I do for you?");
-        System.out.println("___________________________________");
         System.out.println(logo);
+        System.out.println("___________________________________");
 
+        // main loop
         while (true) {
             String input = sc.nextLine().toLowerCase().trim();   
             if (input.equals("bye")) {
@@ -22,9 +27,19 @@ public class Chad {
                 System.out.println("\tBye. Hope to see you again soon!");
                 System.out.println("\t___________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("\t___________________________________");
+                for (int i = 0; i < taskCounter; i++) {
+                    System.out.println("\t" + (i + 1) + ". " + taskList[i]);
+                }
+                System.out.println("\t___________________________________");
+                continue;
             }
+
+            taskList[taskCounter] = input;
+            taskCounter++;
             System.out.println("\t___________________________________");
-            System.out.println("\t"+input);
+            System.out.println("\tadded: " + input);
             System.out.println("\t___________________________________");
         }
         sc.close();
