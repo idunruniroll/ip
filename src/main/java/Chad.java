881ba7a43717e0707170877f6ea3637463316649
idunruniroll <taan.yi.qian@gmail.com>
2026-01-22 00:55:00 +0800

    Level-1 echo with separators and updated greeting code

--- src/main/java/Chad.java
+++ src/main/java/Chad.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class Chad {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String logo = "       _               _ \n"
                 + "      | |             | |\n"
                 + "   ___| |__   __ _  __| |\n"
                 + "  / __| '_ \\ / _` |/ _` |\n"
                 + " | (__| | | | (_| | (_| |\n"
                 + "  \\___|_| |_|\\__,_|\\__,_|\n";
-        System.out.println("Hello! I'm Chad\nWhat can I do for you?\n" + logo + "\nBye. Hope to see you agagin soon");
+        System.out.println("___________________________________");
+        System.out.println("Hello! I'm Chad");
+        System.out.println("What can I do for you?");
+        System.out.println("___________________________________");
+        System.out.println(logo);
+
+        while (true) {
+            String input = sc.nextLine().toLowerCase().trim();   
+            if (input.equals("bye")) {
+                System.out.println("\t___________________________________");
+                System.out.println("\tBye. Hope to see you again soon!");
+                System.out.println("\t___________________________________");
+                break;
+            }
+            System.out.println("\t___________________________________");
+            System.out.println("\t"+input);
+            System.out.println("\t___________________________________");
+        }
+        sc.close();
     }
 }
+
