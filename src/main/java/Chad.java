fba2e47adce7c0af289b4b78dc37177b88b99eac
idunruniroll <taan.yi.qian@gmail.com>
2026-01-22 00:39:36 +0800

    Level-0 rename Duke to Chad and add greeting

---
+++ src/main/java/Chad.java
@@ -0,0 +1,11 @@
+public class Chad {
+    public static void main(String[] args) {
+        String logo = "       _               _ \n"
+                + "      | |             | |\n"
+                + "   ___| |__   __ _  __| |\n"
+                + "  / __| '_ \\ / _` |/ _` |\n"
+                + " | (__| | | | (_| | (_| |\n"
+                + "  \\___|_| |_|\\__,_|\\__,_|\n";
+        System.out.println("Hello! I'm Chad\nWhat can I do for you?\n" + logo + "\nBye. Hope to see you agagin soon");
+    }
+}
