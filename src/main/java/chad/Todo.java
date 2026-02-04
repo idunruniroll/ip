bb5eb236b453222e1c0e9c686e25f55e871d8532
idunruniroll <taan.yi.qian@gmail.com>
2026-02-04 14:41:06 +0800

    Revert "A-CheckStyle: set up and fix style checks"
    This reverts commit 4752530a3f782e42d13fef9506291ee6d13eaa9a.

--- src/main/java/chad/Todo.java
+++ src/main/java/chad/Todo.java
@@ -1,18 +1,6 @@
 package chad;
 
-/**
- * Represents a simple todo task without any date/time constraints.
- * 
- * @author Yi Qian
- * @version 1.0
- * @since 2025-01-30
- */
 public class Todo extends Task {
-    /**
-     * Creates a todo task.
-     *
-     * @param description Task description.
-     */
     public Todo(String description) {
         super(description);
     }
